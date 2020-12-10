a68c863ae71a80929f21182b561e45170bca9045
noajam <nrj006@uark.edu>
2020-12-10 15:32:14 -0600

    Revert "Noah J., Noah B. Trenton, Christina, Calvin"
    This reverts commit d4ec2ff2227b63f307f7d7db5c0df7ae8fe28d45.

--- src/main/resources/static/scripts/master.js
+++ src/main/resources/static/scripts/master.js
@@ -5,13 +5,6 @@ document.addEventListener("DOMContentLoaded", () => {
 	}
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-	const goBackActionElement = getGoBackActionElement();
-	if (goBackActionElement != null) {
-		goBackActionElement.addEventListener("click", goBackActionClickHandler);
-	}
-})
-
 // AJAX
 function ajaxGet(resourceRelativeUri, callback) {
 	return ajax(resourceRelativeUri, "GET", null, callback);
@@ -176,10 +169,6 @@ function getSignOutActionElement() {
 	return document.getElementById("signOutImage");
 }
 
-function getGoBackActionElement() {
-	return document.getElementById("goBackImage");
-}
-
 function getErrorMessageContainerElement() {
 	return document.getElementById("error");
 }
@@ -195,7 +184,7 @@ function signOutActionClickHandler() {
 		if ((callbackResponse.data != null)
 			&& (callbackResponse.data.redirectUrl != null)
 			&& (callbackResponse.data.redirectUrl !== "")) {
-
+	
 			window.location.replace(callbackResponse.data.redirectUrl);
 		} else {
 			window.location.replace("/");
@@ -203,9 +192,3 @@ function signOutActionClickHandler() {
 	});
 }
 //End sign out
-
-//Back button function
-function goBackActionClickHandler() {
-	window.location.assign("/mainMenu");
-}
-//End back button function
